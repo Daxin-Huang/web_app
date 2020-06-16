25d5295676e3a49e0cab9c92cb3c2595392e2a95
hdx-desktop-ubuntu <gxllhdx0119+du@gmail.com>
2020-06-16 21:23:44 +0800

    添加了用户的限制并修改了页面的样式

--- learning_log/views.py
+++ learning_log/views.py
@@ -1,27 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
+def check_topic_owner(topic,request):
+    if topic.owner != request.user:
+        raise Http404
+
 def index(request):
     return render(request, 'learning_log/index.html')    
 
+@login_required
 def topics(request):
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_log/topics.html', context)
 
+@login_required
 def topic(request, topic_id): # 特定的topic
     topic = Topic.objects.get(id=topic_id)
+    
+    check_topic_owner(topic, request)
+
     # 通过外键来读取entry这一属性
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_log/topic.html', context)
 
+@login_required
 def new_topic(request):
     """添加新主题"""
     # 未提交数据： 创建一个新表单
@@ -31,12 +42,15 @@ def new_topic(request):
         # POST提交的数据,对数据进行处理
         form = TopicForm(request.POST)
         if form.is_valid():
-            # 这里是直接保存，因为
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return HttpResponseRedirect(reverse('learning_log:topics'))
+
     context = {'form': form}
     return render(request, 'learning_log/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """在特定的主题中添加新条目"""
     topic = Topic.objects.get(id=topic_id)
@@ -44,6 +58,7 @@ def new_entry(request, topic_id):
         # 未提交数据,创建一个空表单
         form = EntryForm()
     else:
+        check_topic_owner(topic, request)
         # POST提交的数据,对数据进行处理
         form = EntryForm(data=request.POST)
         if form.is_valid():
@@ -55,10 +70,13 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_log/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """编辑既有条目"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    
+    check_topic_owner(topic, request)
 
     if request.method != 'POST':
         # 初次请求， 使用当前条目填充表单
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.contrib.auth import logout, authenticate
+from django.contrib.auth import login,logout, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
